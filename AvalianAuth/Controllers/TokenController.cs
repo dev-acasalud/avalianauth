d8b5471666ea1335de4790ddabb946cd39e93a05
fede <fzurita@acasalud.com.ar>
2020-08-18 00:48:08 -0300

    first commit

---
+++ AvalianAuth/Controllers/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AvalianAuth.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace AvalianAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public TokenController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var jwt = new JwtService(_config);
+            var token = jwt.GenerateSecurityToken("prueba@email.com");
+            return token;
+        }
+    }
+}
